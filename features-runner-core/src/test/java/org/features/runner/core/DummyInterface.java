b9070560819c93093b398c4e8bda53f45f7b178c
Damien Metzler <damien.metzler@leroymerlin.fr>
2011-07-20 23:49:10 +0200

    Add licences info, prepare package for public publishing

--- features-runner-core/src/test/java/org/features/runner/core/DummyInterface.java
+++ features-runner-core/src/test/java/org/features/runner/core/DummyInterface.java
@@ -1,3 +1,15 @@
+/*
+* Copyright (c) 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* 	  Damien Metzler - outsourced from Nuxeo to features-runner
+*     Nuxeo - initial API and implementation
+*/
 package org.features.runner.core;
 
 public interface DummyInterface {
